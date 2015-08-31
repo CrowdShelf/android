7bb92a26b1f901009034b345e64aa2e11cce5ff5
Markus Lund <markuslund92@hotmail.com>
2015-08-31 18:52:30 +0200

    Added possibility to send email using gmail.

--- app/src/main/java/ntnu/stud/markul/crowdshelf/MainActivity.java
+++ app/src/main/java/ntnu/stud/markul/crowdshelf/MainActivity.java
@@ -1,18 +1,18 @@
 package ntnu.stud.markul.crowdshelf;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
-import java.util.Scanner;
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    private GMailSender gMailSender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,5 +53,26 @@ public class MainActivity extends ActionBarActivity {
     public void openScanner(View view) {
         Intent intent = new Intent(this, ScannerActivity.class);
         startActivity(intent);
+    }
+
+    public void sendMailButtonPressed(View view){
+        sendMail("This is a subject", "This is the main text of the email", "kongen@slottet.no", "markuslund92@hotmail.com");
+    }
+
+    //Example of how mail can be sent
+    public void sendMail(String subject, String body, String senderEmail, String recipients) {
+        try{
+
+            if (gMailSender == null)
+                setUpGmailSender("kundestyrt", "crowdshelfmail@gmail.com");
+
+            gMailSender.sendMail(subject, body, senderEmail, recipients);
+        }catch (Exception e){
+            Log.e("SendMail", e.getMessage(), e);
+        }
+    }
+
+    private void setUpGmailSender(String password, String user) {
+        gMailSender = new GMailSender(user, password);
     }
 }
