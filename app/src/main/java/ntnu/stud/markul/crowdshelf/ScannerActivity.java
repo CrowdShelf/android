5b88b14768f781e1d8eab087ae95f1e3156c3227
Morten <morteano@stud.ntnu.no>
2015-08-31 13:05:32 +0200

    Stored ISBN in a string variable called ISBN

--- app/src/main/java/ntnu/stud/markul/crowdshelf/ScannerActivity.java
+++ app/src/main/java/ntnu/stud/markul/crowdshelf/ScannerActivity.java
@@ -1,11 +1,13 @@
 package ntnu.stud.markul.crowdshelf;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
 
@@ -40,7 +42,12 @@ public class ScannerActivity extends Activity implements ZXingScannerView.Result
     public void handleResult(Result rawResult) {
         // Do something with the result here
         System.out.println("handleResult");
-        Log.v(TAG, rawResult.getText()); // Prints scan results
-        Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+        //Log.v(TAG, rawResult.getText()); // Prints scan results
+        //Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+        String ISBN = rawResult.getText().toString(); // Stores the ISBN in a variable
+        Toast.makeText(getApplicationContext(), "Book scanned: " + ISBN, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent); // Return to start page
     }
 }
