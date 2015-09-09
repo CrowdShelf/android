512aa7a5ecb3feefe315a60e63af0bff1060bf24
Morten <morteano@stud.ntnu.no>
2015-09-09 14:04:33 +0200

    Added dummy onClick functions to prevent error

--- app/src/main/java/com/crowdshelf/app/activities/ScanResultActivity.java
+++ app/src/main/java/com/crowdshelf/app/activities/ScanResultActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,10 +21,13 @@ import ntnu.stud.markul.crowdshelf.R;
  * Created by MortenAlver on 07.09.2015.
  */
 public class ScanResultActivity extends Activity {
+
+    private String ISBN = "";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String ISBN = intent.getStringExtra("ISBN");
+        ISBN = intent.getStringExtra("ISBN");
         setContentView(R.layout.activity_scan_result);
 
         Log.i("ScanResult", ISBN);
@@ -52,5 +56,23 @@ public class ScanResultActivity extends Activity {
 
 
         }
+    }
+
+    public void addButtonClick(View view) {
+        // Add book to database, and change to my shelf
+        Toast.makeText(ScanResultActivity.this, "Add book: " + ISBN, Toast.LENGTH_SHORT).show();
+    }
+
+    public void borrowButtonClick(View view) {
+        // Borrow book
+        Toast.makeText(ScanResultActivity.this, "Borrow book: " + ISBN, Toast.LENGTH_SHORT).show();
+    }
+
+    public void returnButtonClick(View view) {
+        Toast.makeText(ScanResultActivity.this, "Return book: " + ISBN, Toast.LENGTH_SHORT).show();
+    }
+
+    public void removeButtonClick(View view) {
+        Toast.makeText(ScanResultActivity.this, "Remove book: " + ISBN, Toast.LENGTH_SHORT).show();
     }
 }
