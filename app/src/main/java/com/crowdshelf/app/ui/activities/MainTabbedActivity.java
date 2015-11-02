477fbe17bcaed333809932b8676a3ca6fbc6bfba
Markus Lund <markuslund92@hotmail.com>
2015-11-02 16:22:09 +0100

    bugfix

--- app/src/main/java/com/crowdshelf/app/ui/activities/MainTabbedActivity.java
+++ app/src/main/java/com/crowdshelf/app/ui/activities/MainTabbedActivity.java
@@ -418,8 +418,6 @@ public class MainTabbedActivity extends AppCompatActivity implements
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_testing) {
-            Intent intent = new Intent(this, TestingActivity.class);
-            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
